7e80983ee42b90f3988a4aba79a7a0a80bef4d8a
fahu26 <fahimshaikh2606@gmail.com>
2020-04-20 21:58:32 +0530

    Removed unwanted console logs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,4 @@
 
-console.log("Welocome to JS staic files")
-
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
